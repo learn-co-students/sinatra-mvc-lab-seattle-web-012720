a1393979a950dfd9aeb1bf184972bedfd6aca6da
unclezap <zeb.blackwell@gmail.com>
2020-02-20 01:33:35 -0800

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,16 +1,8 @@
-require 'pry'
 class PigLatinizer
     attr_accessor :text
 
-    # def initialize(text="")
-    #     binding.pry
-    #     @text = text
-    # end
-
     def piglatinize(text)
-        # binding.pry
         array = text.split(' ')
-        # binding.pry
         pig_array = array.map do |word|
            if word.scan(/^[aeiouAEIOU]/)[0] != nil
                 word = word + "way"
@@ -18,18 +10,11 @@ class PigLatinizer
                 beginning = word.scan(/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]*/)[0]
                 word = word[beginning.length, word.length] + beginning + "ay"
            end
-            # after_start = word.scan(/[^bcdfghjklmnpqrstvwxyz]/)
+
         end
-            #     word = word + first_letter
-        #     word = word[1,word.length - 1]
-        #     word = word + "ay"
-        # end
+
         pig_array.join(' ')
     end
 
-    # def self.piglatinize
-    # end
 
 end
-
-# binding.pry
